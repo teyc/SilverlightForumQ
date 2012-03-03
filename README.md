8a699735eaff169aa56cbf5abc435350d3790209
Chui Tey <teyc@cognoware.com>
2012-03-04 09:52:29 +1000

    Showing images in the Designer and Blend

---
+++ README.md
@@ -0,0 +1,5 @@
+Silverlight.net Forum Samples
+=============================
+
+Contains code samples used in response to Silverlight.net forum questions
+
---
+++ SilverlightForumQ/Q250404.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace SilverlightForumQ
+{
+    public partial class Q250404 : UserControl
+    {
+        public Q250404()
+        {
+            InitializeComponent();
+        }
+    }
+}
